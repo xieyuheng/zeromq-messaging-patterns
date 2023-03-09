f349d66e6c71f21e8d60d5a60986a02656a40c7f
Xie Yuheng <xyheme@gmail.com>
2023-03-09 20:19:27 +0800

    [load-balancing-broker] note about why `Broker` uses two sockets -- `majordemo` only uses one.

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
-[load-balancing-broker] note about why `Broker` uses two sockets -- `majordemo` only uses one.
-
 [load-balancing-broker] `Broker` quit using `wait` -- use `match` instead
 
 [majordemo-async] Like the `majordemo` but only use `Dealer` and `Router`,
--- src/load-balancing-broker/Broker.ts
+++ src/load-balancing-broker/Broker.ts
@@ -1,5 +1,20 @@
 import type * as Zmq from "zeromq"
 
+/**
+
+   `Broker` uses two router -- `majordemo` only uses one.
+
+   Using one router, we can add `kind` to messages,
+   to distinguish client message from worker message.
+
+   While using two routers, we can already
+   distinguish message by the different routers.
+
+   The interface of client and worker becomes more simple,
+   since we do not need to format `kind` in the message.
+
+**/
+
 export type Broker = {
   frontend: Zmq.Router
   backend: Zmq.Router
