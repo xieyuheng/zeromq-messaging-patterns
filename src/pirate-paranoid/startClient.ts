88db8585c3dccb0efcab81a15321a9fb12866235
Xie Yuheng <xyheme@gmail.com>
2023-03-10 15:50:45 +0800

    up

---
+++ src/pirate-paranoid/startClient.ts
@@ -0,0 +1,10 @@
+type Options = {
+  serverAddress: string
+}
+
+export async function startClient(options: Options) {
+  const {serverAddress} = options
+
+  const who = "client"
+  //
+}
---
+++ src/pirate-paranoid/startServer.ts
@@ -0,0 +1,10 @@
+type Options = {
+  serverAddress: string
+}
+
+export async function startServer(options: Options) {
+  const {serverAddress} = options
+
+    const who = "server"
+  //
+}
